8e9ac4c4a175d7b5e02f4169f90a4e2d40759b5c
chenguoming1 <chenguoming1@gmail.com>
2014-06-27 17:58:31 +0800

    Sync to github

--- sync_to_github/recipes/default.rb
+++ sync_to_github/recipes/default.rb
@@ -1,6 +1,11 @@
 node[:deploy].each do |app_name, deploy_config|
-	app_root = "#{deploy_config[:deploy_to]}/current/forums/cache"
-	directory app_root do
-		mode 0664
-	end
+	dir = "#{deploy_config[:deploy_to]}/current"	
+	message = "Syncing staging to github at #{Time.now.utc}"
+	system "git -C #{dir} add ."
+	system "git -C #{dir} add -u"
+	system "git -C #{dir} commit -m \"#{message}\""
+	system "git -C #{dir} pull origin test"
+	system "git -C #{dir} tag -a #{Time.now.to_i} -m \"Synced at #{Time.now.utc}\""
+	system "git -C #{dir} push --tags origin test"
+	puts "## Done!"
 end
