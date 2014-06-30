84116c7ad002cb3d1d34a6d1bfc3541086460784
chenguoming1 <chenguoming1@gmail.com>
2014-06-30 15:35:07 +0800

    Staging sync

--- sync_to_github/recipes/default.rb
+++ sync_to_github/recipes/default.rb
@@ -6,8 +6,9 @@ node[:deploy].each do |app_name, deploy_config|
 	system "cd #{dir} && git add ."
 	system "cd #{dir} && git add -u"
 	system "cd #{dir} && git commit -m \"#{message}\""
-	system "cd #{dir} && git pull origin deploy"
+	system "cd #{dir} && git branch deploy -u origin/staging_deploy"
+	#system "cd #{dir} && git pull origin staging_deploy"
 	system "cd #{dir} && git tag -a #{Time.now.to_i} -m \"Synced at #{Time.now.utc}\""
-	system "cd #{dir} && git push --tags origin deploy"
+	system "cd #{dir} && git push --tags origin staging_deploy"
 	puts "## Done!"
 end
