b1c7e6bc57780a67f081ca7cc74dde6db33a44a2
Aung Bo Naing <chenguoming1@gmail.com>
2014-07-04 10:21:07 +0800

    added zend guard loader config recipe

--- install_zend_guard_loader/attributes/default.rb
+++ install_zend_guard_loader/attributes/default.rb
@@ -1,3 +1,4 @@
 default['php']['ext_dir'] = '/usr/lib64/php/modules'
 default['php']['ext_conf_dir'] = '/etc/php.d'
-default['php']['zend_extension_conf_vars'] = {'zend_extension' => "#{php['ext_dir']}/ZendGuardLoader.so", "zend_loader.enable" => 1}
+default['php']['extension_template'] = 'extension.ini.erb'
+default['php']['zend_extension']['config_vars'] = {'zend_extension' => "#{php['ext_dir']}/ZendGuardLoader.so", "zend_loader.enable" => 1}
--- install_zend_guard_loader/recipes/default.rb
+++ install_zend_guard_loader/recipes/default.rb
@@ -3,3 +3,11 @@ cookbook_file "ZendGuardLoader.so" do
   mode "0755"
   action :create
 end
+
+template "#{node['php']['ext_conf_dir']}/zend_guard_loader.ini" do
+  source node['php']['extension_template']
+  owner 'root'
+  group 'root'
+  mode 00644
+  variables(node['zend_extension']['config_vars'])
+end
